0abdfdef5120f02a692fab559adba92d5cd138fd
leonel1414 <leoo9211@hotmail.com>
2025-09-11 21:04:18 -0300

    continuar

--- async-await.js
+++ async-await.js
@@ -4,8 +4,21 @@ const promise = new Promise((resolve, reject) => {
     }, 2000);
 });
 
-promise
-    .then((promise) => console.log(promise))
-    .catch((error) => console.error(error));
+//promise
+//    .then((promise) => console.log(promise))
+//    .catch((error) => console.error(error));
 
 
+async function main(){
+    try{
+        const result = await promise;
+        console.log(result);
+
+    } catch (error){
+        console.error(error);
+    }
+};
+
+main();
+
+//6:18
