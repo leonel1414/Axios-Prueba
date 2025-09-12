abb15f10feab3284cea992808a6b95f62188c342
leonel1414 <leoo9211@hotmail.com>
2025-09-12 20:06:24 -0300

    promesas

---
+++ promesas.js
@@ -0,0 +1,14 @@
+/*const promise = new Promise((resolve, reject) =>{
+    setTimeout(() =>{
+        resolve('ok');
+    },1000);
+});
+
+promise
+.then((response) =>{
+    console.log(response);
+})
+.catch((error) =>{
+    console.error(error);
+});
+*/
