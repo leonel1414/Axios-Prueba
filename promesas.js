c32aeb83df90e2414fe9a078c89769e4934db50c
leonel1414 <leoo9211@hotmail.com>
2025-09-15 19:56:28 -0300

    continuando con promesas

--- promesas.js
+++ promesas.js
@@ -12,3 +12,27 @@ promise
     console.error(error);
 });
 */
+const random = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
+const multiplicar = (num1, num2) =>{
+    
+    if(typeof num1 != 'number' || typeof num2 != 'number'){
+        return Promise.reject("Los parametros tiene que ser números")
+    }
+
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            resolve({
+                num1, num2, resultado: num1 * num2,
+            });
+        },random(500, 1500));
+    });
+};
+
+multiplicar(2,3)
+.then((response) =>{
+    console.log(`${response.num1} * ${response.num2} = ${response.resultado} `);
+})
+.catch((error) =>{
+    console.log(error);
+});
