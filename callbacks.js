a7176ff262b5a3fdc10a03eb8571b8ce42de6234
leonel1414 <leoo9211@hotmail.com>
2025-09-16 22:56:25 -0300

    modificaciones

--- callbacks.js
+++ callbacks.js
@@ -7,3 +7,4 @@ const calculadora = (num1,num2,callback) =>{
 const resultado = calculadora(2,2,calculadora());
 console.log(resultado);
 
+//https://www.youtube.com/watch?v=oxG5nTUQy9M&list=PLxXpfdBOyZ_VOq9nU9G1zq2hsIQ27Vnhd&index=6
