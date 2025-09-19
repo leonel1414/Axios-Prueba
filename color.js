1dc48d721cbccd91353a75c627bb164a18cdf8c2
leonel1414 <leoo9211@hotmail.com>
2025-09-19 11:10:53 -0300

    arrays

--- color.js
+++ color.js
@@ -1,1 +1,34 @@
-//https://www.youtube.com/watch?v=IEqiTi67ZNg&list=PLxXpfdBOyZ_VOq9nU9G1zq2hsIQ27Vnhd&index=7
+const autos = [
+    { marca: "ford", modelo: "fiesta", año:2010, color: "rojo"},
+    { marca: "chevrolet", modelo: "onix", año:2011, color: "azul"},
+    { marca: "toyota", modelo: "corolla", año:2012, color: "blanco"},
+    { marca: "honda", modelo: "civic", año:2013, color: "negro"},
+    { marca: "hyundai", modelo: "elantra", año:2013, color: "rojo"},
+];
+
+/*
+function contarPorColor(){
+    let contador = 0;
+
+    autos.forEach((auto) =>{
+        if(auto.color.toLocaleLowerCase() == colorBuscado.toLocaleLowerCase()){
+            contador++;
+        }
+    });
+    console.log(`Hay ${contador} autos de color ${colorBuscado}`);
+}
+*/
+
+function contarPorColor(){
+    let contador = 0;
+
+    autos.forEach((auto) =>{
+        if(auto.color.toLocaleLowerCase() == colorBuscado.toLocaleLowerCase()){
+            contador++;
+        }
+    });
+    console.log(`Hay ${contador} autos de color ${colorBuscado}`);
+}
+
+
+colorBuscado('rojo');
