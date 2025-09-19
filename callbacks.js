d833d4cf3fc6cfadd9d5dbec0fe7b7803a951d9e
leonel1414 <leoo9211@hotmail.com>
2025-09-19 08:21:14 -0300

    callbacks

--- callbacks.js
+++ callbacks.js
@@ -1,10 +1,23 @@
-const multiplicar = (num1,num2) => num1 * num2;
+/*const multiplicar = (num1,num2) => num1 * num2;
 
 const calculadora = (num1,num2,callback) =>{
     return callback(num1,num2);
 };
 
-const resultado = calculadora(2,2,calculadora());
+const resultado = calculadora(2,2,multiplicar());
 console.log(resultado);
+*/
 
-//https://www.youtube.com/watch?v=oxG5nTUQy9M&list=PLxXpfdBOyZ_VOq9nU9G1zq2hsIQ27Vnhd&index=6
+// ---
+
+const multiplicar = (num1, num2, callback) => {
+    setTimeout(() =>{
+        callback( num1, num2, num1 * num2)
+    },2000)
+};
+
+const mostrar = (a, b, c) =>{
+    console.log(`${a} * ${b} = ${c}`);
+};
+
+multiplicar(2,2,mostrar);
