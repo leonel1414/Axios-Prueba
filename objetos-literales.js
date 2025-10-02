c2b1cef7104b8a47fcd5e03bbf28be22b607291b
leonel1414 <leoo9211@hotmail.com>
2025-10-02 08:30:21 -0300

    objectos literales

---
+++ objetos-literales.js
@@ -0,0 +1,11 @@
+//const persona = new Object();
+
+//persona.nombre = 'Leo';
+//persona.apellido = 'Girett';
+
+const persona = {
+    nombre: 'Leonel',
+    apellido: 'Girett'
+};
+
+console.log(persona);
