25310591e9b8ed655c2f22558b031401b8dcba14
leonel1414 <leoo9211@hotmail.com>
2025-10-03 16:44:30 -0300

    modificaciones

--- array.js
+++ array.js
@@ -1,11 +1,11 @@
 const numeros = [100,50,20.99,12.5,75];
 
 numeros.forEach((numero, index) =>{
-    numeros[index] * 1.1;
+    numeros[index] = Number((numeros * 1.1).toFixed(2));
 
     // numero *= 1.1;
-
-    console.log(`El numero aumento es: ${numero.toFixed(2)}`);
+    
+    console.log(`El numero aumento es: ${numeros[index]}`);
 
 });
 
